4d8918eb1b2ead482f338fcb49d02d3538d58317
MasterX01 <aakashsaxena14@gmail.com>
2021-04-21 22:32:06 +0530

    [Akash] ADD. code for passing name in body

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,10 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 @RestController
 @RequestMapping({"/hello", ""})
@@ -25,5 +29,10 @@ public class HelloWorldController {
 	@GetMapping("/param/{name}")
 	public String showHello(@PathVariable String name) {
 		return "Hello " + name + " from BridgeLabz.";
+	}
+	
+	@PostMapping("/post")
+	public String showHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz.";
 	}
 }
